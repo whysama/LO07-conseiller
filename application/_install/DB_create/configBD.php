b0cdc38fe42106e4f22b7e2778ebe3eca38c6ec7
whysama <zhou.shenyoung.young@gmail.com>
2014-06-16 11:22:44 +0200

    finish v3 with clean fiche but data problem and select problem

--- application/_install/DB_create/configBD.php
+++
@@ -1,6 +0,0 @@
-<?php
-	//配置参数
-	$user = "root";
-	$password = "root";
-	$dataSourceName = "mysql:host=localhost;dbname=young"
-?>
--- application/views/Etudiant/index.php
+++
@@ -1,9 +0,0 @@
-<div id="user">
-    <h2>Etudiant</h2>
-    <?php
-            echo $etudiant[0]->prenom." ".$etudiant[0]->nom."<br>";
-            echo "Votre conseiller : ";
-            echo $conseiller[0]->prenom." ".$conseiller[0]->nom."<br>";
-    ?>
-
-</div>
